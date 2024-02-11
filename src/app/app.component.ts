c2faa56b1dd5bc6f5a3611efddff98e394a15449
Iosif1990 <152786810+Iosif1990@users.noreply.github.com>
2024-02-11 17:00:46 +0100

    Se logra guardar las respuestas del formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,15 @@ import { ContadorComponent } from './components/contador/contador.component';
 import { NoticiasComponent } from './components/noticias/noticias.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ContadorComponent, NoticiasComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, ContadorComponent, NoticiasComponent],
+    templateUrl:'./app.component.html',
+    styleUrls: ['./app.component.css'],
+  })
+  
 export class AppComponent {
   title = 'primeraAppStandalone';
 }
+
+
--- src/app/components/noticias/noticias.component.ts
+++ src/app/components/noticias/noticias.component.ts
@@ -1,55 +1,30 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  selector: 'app-noticias',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl:'./noticias.component.html',
+  styleUrls: ['./noticias.component.css'],
 })
-export class BlogComponent {
-  arrNoticias: any[] = [
-    {
-      titulo: 'Noticia 1',
-      imagen: 'https://ejemplo.com/imagen1.jpg',
-      texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      fecha: '2022-01-01'
-    },
-    {
-      titulo: 'Noticia 2',
-      imagen: 'https://ejemplo.com/imagen2.jpg',
-      texto: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      fecha: '2022-01-02'
-    }
-  ];
-
-  newNoticias: any = {
-    titulo: '',
-    imagen: '',
-    texto: '',
-    fecha: ''
-  };
-
-  guardar() {
-    if (this.validarNoticia(this.newNoticias)) {
-      this.arrNoticias.push(this.newNoticias);
-      this.resetearFormulario();
-    } else {
-      alert('Por favor, complete todos los campos del formulario.');
-    }
-  }
-
-  validarNoticia(noticia: any): boolean {
-    return noticia.titulo && noticia.imagen && noticia.texto && noticia.fecha;
-  }
-
-  resetearFormulario() {
-    this.newNoticias = {
-      titulo: '',
-      imagen: '',
-      texto: '',
-      fecha: ''
-    };
-  }
+export class NoticiasComponent {
+  arrNoticias: any[] = [];
+  
+  newNoticia: any = {
+  'titulo':"",
+  'imagen':"",
+  'texto':"",
+  'fecha':"",
 }
-
+guardar(){
+  this.arrNoticias.push(this.newNoticia);
+  console.log(this.arrNoticias)
+  this.newNoticia = {
+    'titulo':"",
+    'imagen':"",
+    'texto':"",
+    'fecha':"",
+}
+}
+}
